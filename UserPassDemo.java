1e0cf889ea4079c2233780c1a2145d82ebe9a36c
Syed-Kashif-Ahmed <116105683+Syed-Kashif-Ahmed@users.noreply.github.com>
2022-10-28 10:25:22 +0530

    Update UserPassDemo.java

--- UserPassDemo.java
+++ UserPassDemo.java
@@ -8,17 +8,22 @@ public class UserPassDemo {
 		// TODO Auto-generated method stub
 		String password, confirm_password;  // we have created to variables to accept user input and store
 		Scanner d = new Scanner(System.in); // Scanner for accepting input
-		System.out.println("Enter your password :");
+		System.out.println("Enter your password :"); // A message for the user to enter the password
 		password = d.next(); 		// First accepting the password
-		System.out.println("Re-Enter your password :");
+		System.out.println("Re-Enter your password :");  // A message for the user to re-enter the password
 		confirm_password = d.next(); // Storing the re-entered password in confirm_password
 		if (password .equals(confirm_password)) // Checking whether both the passwords match
 		{
+			//If the condition is true this statement is executed
+			//which means if both the passwords match then this statement is executed
 			System.out.println("Your password matched ");
-			
 		}
 		else
+		{
+			//If the condition is false this statement is executed
+			//which means if both the passwords didn't match then this statement is executed
 			System.out.println("Your password Did not matched ");
+		}
 	}
 
 }
