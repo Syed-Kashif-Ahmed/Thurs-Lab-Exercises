719be8521fa77b1fbae20675d190601c60f4e926
Syed-Kashif-Ahmed <116105683+Syed-Kashif-Ahmed@users.noreply.github.com>
2022-10-28 10:20:59 +0530

    Update EvenOddDemo.java

--- EvenOddDemo.java
+++ EvenOddDemo.java
@@ -6,15 +6,17 @@ public class EvenOddDemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int n; 
-		Scanner s = new Scanner(System.in);
-		System.out.println("Enter a value : ");
-		n = s.nextInt();
+		int n; // Creating a variable n of integer type
+		Scanner s = new Scanner(System.in); // Scanner for accepting input
+		System.out.println("Enter a value : ");  // A message for the user to enter a value
+		n = s.nextInt();  // storing the user input in variable a
 		if (n % 2 == 0) 
-			// the input of the user is stored in n..n is divided by 2 and if the remainder is 0 then it is even else odd
-			System.out.println("The given number " + n + " is even");
+		// the input of the user is stored in n..n is divided by 2 and if the remainder is 0 then it is even else odd
+			System.out.println("The given number " + n + " is even"); 
+			//If the condition is true this statement is executed
 		else
 			System.out.println("The given number " + n + " is odd");
+			//If the condition is false this statement is executed
 	}
 
 }
